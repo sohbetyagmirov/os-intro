6cd98bb1ab803fbd6e0fc66e0142ee3e04de52d5
sohbetyagmirov <1032224647@pfur.ru>
2023-04-06 10:20:00 +0300

    lab 9

---
+++ labs/lab09/hello.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+HELLO=HEllo
+function hello {
+local HELLO=World
+echo $HELLO
+}
+echo $HELLOecho $HELLOecho $HELLO
+hello
+function hello {
+local HELLO=World
+echo $HELLO
+}
+echo $HELLOecho $HELLOecho $HELLO
+hello
+function hello {
+local HELLO=World
+echo $HELLO
+}
+echo $HELLOecho $HELLOecho $HELLO
+hello
